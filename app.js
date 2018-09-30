c258c16f8abe9223d42e0dc232293e2f435cbba3
Shashi <shashikunal@gmail.com>
2018-09-30 15:36:30 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ const fs = require('fs');
 
 // fs.writeFileSync('writeme.txt' , readme);
 
+//  fs.readFile('./readme.txt' , 'utf8' , function(err , data){
+//     console.log(data);
+//     fs.writeFile('writeme.txt' , data);
+// })
+
+// console.log('test');
+
+// fs.mkdirSync('stuff');
+
+// fs.mkdir('stuff' , ()=>{
+//     fs.readFile('readme.txt', 'utf8' , (err , data)=>{
+//       fs.writeFile('./stuff/writeme.txt' , data);
+//     });
+// })
